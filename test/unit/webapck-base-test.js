41ab5495cf525e06ea3b69a248c94249fda09c78
Shinedi <fubodi@jd.com>
2020-07-28 20:07:09 +0800

    chore:update travis-2

--- test/unit/webapck-base-test.js
+++ test/unit/webapck-base-test.js
@@ -4,10 +4,10 @@ describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.js');
     console.log(baseConfig)
     it('entry', ()=> {
-        assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.js') > -1, true);
-        assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.js') > -1, true);
+        assert.equal(baseConfig.entry.index.indexOf('builder-webpack-0728/test/smoke/template/src/index/index.js') > -1, true);
+        assert.equal(baseConfig.entry.search.indexOf('builder-webpack-0728/test/smoke/template/src/search/index.js') > -1, true);
     })
     it('output', ()=> {
-        assert.equal(baseConfig.output.path.indexOf('builder-webpack/test/smoke/template/dist') > -1, true);
+        assert.equal(baseConfig.output.path.indexOf('builder-webpack-0728/test/smoke/template/dist') > -1, true);
     })
 })
