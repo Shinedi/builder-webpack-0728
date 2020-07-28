cc28b09305bacab6e7ab0a2cbce85e5eda233c8b
Shinedi <fubodi@jd.com>
2020-07-28 20:00:55 +0800

    chore:update travis

--- test/unit/webapck-base-test.js
+++ test/unit/webapck-base-test.js
@@ -4,10 +4,10 @@ describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.js');
     console.log(baseConfig)
     it('entry', ()=> {
-        assert.equal(baseConfig.entry.index, '/Users/fubodi/learn_dirctory/webpack/webpack-test/builder-webpack/test/smoke/template/src/index/index.js');
-        assert.equal(baseConfig.entry.search, '/Users/fubodi/learn_dirctory/webpack/webpack-test/builder-webpack/test/smoke/template/src/search/index.js');
+        assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.js') > -1, true);
+        assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.js') > -1, true);
     })
     it('output', ()=> {
-        assert.equal(baseConfig.output.path, '/Users/fubodi/learn_dirctory/webpack/webpack-test/builder-webpack/test/smoke/template/dist');
+        assert.equal(baseConfig.output.path.indexOf('builder-webpack/test/smoke/template/dist') > -1, true);
     })
 })
